9b7a72c4255b5ff55bfe574494697b9808a96809
wangshan <wangshan@chenrunsoft.com>
2016-10-12 15:16:11 +0800

    init

---
+++ LCTools/LCTools/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LCTools
+//
+//  Created by 王陕 on 16/10/12.
+//  Copyright © 2016年 王陕. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LCTools/LCTools/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LCTools
+//
+//  Created by 王陕 on 16/10/12.
+//  Copyright © 2016年 王陕. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
