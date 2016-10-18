049d8c68fa2e92198504ae3a8dc59b1f0870f231
wangshan <wangshan@chenrunsoft.com>
2016-10-18 20:06:30 +0800

    commit

--- LCTools/LCTools/ViewController.m
+++ LCTools/LCTools/ViewController.m
@@ -17,9 +17,39 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    
+    hano(3, 1, 2, 3);
     
 }
+
+
+#pragma mark - 递归算法
+
+//阶乘
+int recursive(int i)
+{
+    int sum = 0;
+    if (0 == i)
+        return (1);
+    else
+        sum = i * recursive(i-1);
+    return sum;
+}
+
+
+void hano(int n, int p1, int p2, int p3)
+{
+    if (n == 1) {
+        NSLog(@"%d -> %d", p1,p3);
+    }else{
+        
+        hano(n-1, p1, p3, p2);
+        NSLog(@"%d -> %d", p1,p3);
+        hano(n-1, p2, p1, p3);
+    }
+    
+}
+    
+
 
 
 @end
